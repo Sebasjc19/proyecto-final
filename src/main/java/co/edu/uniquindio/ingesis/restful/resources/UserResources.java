2eb96d95806de99a29d33ad2e4ed00665c582492
Juandque <juanm.duqueg1@uqvirtual.edu.co>
2025-02-24 20:03:11 -0500

    Crea metodo de obtencion de usuarios

--- src/main/java/co/edu/uniquindio/ingesis/restful/resources/UserResources.java
+++ src/main/java/co/edu/uniquindio/ingesis/restful/resources/UserResources.java
@@ -1,8 +1,12 @@
 package co.edu.uniquindio.ingesis.restful.resources;
+import co.edu.uniquindio.ingesis.restful.dtos.usuarios.ObtainUsersResponse;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Path("/users")
@@ -17,5 +21,13 @@ public class UserResources {
         }
         // TODO cambiar por Response.create
         return Response.ok("Usuario con ID " + id + " eliminado con éxito.").build();
+    }
+
+    @GET
+    public Response getUsers(){
+        List<ObtainUsersResponse> responseBody = new ArrayList<>();
+        //Consultar usuarios en la base de datos
+        responseBody.add(new ObtainUsersResponse("James","Smith","Jamesito", LocalDate.of(2002, 05, 17), "3214560987"));
+        return Response.ok(responseBody).build();
     }
 }
