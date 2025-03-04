b82117ab9d7c6569f07f06d028ddc32fd1a1f47f
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2025-03-04 14:11:12 -0500

    Creación de las distintas entidades del modelo de negocio

---
+++ src/main/java/co/edu/uniquindio/ingesis/restful/domain/Report.java
@@ -0,0 +1,27 @@
+package co.edu.uniquindio.ingesis.restful.domain;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "report")
+public class Report extends PanacheEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String content;
+    private LocalDate creationDate;
+    @NotNull
+    private Long groupId;
+    @NotNull
+    private Long professorId;
+}
