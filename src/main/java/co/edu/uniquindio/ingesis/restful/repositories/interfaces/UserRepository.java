87fdc83b8596bb4239a56e02d527c5261ea12d32
Juandque <juanm.duqueg1@uqvirtual.edu.co>
2025-03-06 14:14:05 -0500

    crea repositorios

---
+++ src/main/java/co/edu/uniquindio/ingesis/restful/repositories/interfaces/UserRepository.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.ingesis.restful.repositories.interfaces;
+
+import co.edu.uniquindio.ingesis.restful.domain.User;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends PanacheRepository<User> {
+    Optional<User> finByEmail(String email);
+}
