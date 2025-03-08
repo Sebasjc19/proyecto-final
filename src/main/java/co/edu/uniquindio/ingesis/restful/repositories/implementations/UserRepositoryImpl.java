49adeb13fb25707c5cd8435b1403cc4ad07ef61f
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2025-03-08 17:38:03 -0500

    Corrección de falta de anotacion para la clase UserRepositoryImpl

--- src/main/java/co/edu/uniquindio/ingesis/restful/repositories/implementations/UserRepositoryImpl.java
+++ src/main/java/co/edu/uniquindio/ingesis/restful/repositories/implementations/UserRepositoryImpl.java
@@ -2,9 +2,11 @@ package co.edu.uniquindio.ingesis.restful.repositories.implementations;
 
 import co.edu.uniquindio.ingesis.restful.domain.User;
 import co.edu.uniquindio.ingesis.restful.repositories.interfaces.UserRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.Optional;
 
+@ApplicationScoped
 public class UserRepositoryImpl implements UserRepository {
     @Override
     public Optional<User> finByEmail(String email) {
